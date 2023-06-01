abb63fedf8b35272775ba3064f745176522d302a
annamaynard <anna.maynard@gmail.com>
2023-06-01 12:21:27 -0400

    css, views, random, settings updates

--- music/forms.py
+++ music/forms.py
@@ -3,19 +3,19 @@ from .models import Album
 from .models import Artist
 from .models import Song
 
+
 class ArtistForm(forms.ModelForm):
     class Meta:
         model = Artist
         fields = ('artist', 'genre',)
         
-        
-        
+
 class AlbumForm(forms.ModelForm):
     class Meta:
         model = Album
-        fields = ('artist','title','genre',)
-        widgets = {'image': forms.ClearableFileInput(attrs={'accept': 'image/*'),
-        }
+        fields = ('artist', 'title', 'genre',)
+        widgets = {'image': forms.ClearableFileInput(attrs={'accept': 'image/*'}),
+    }
 
 
 class SongForm(forms.ModelForm):
--- music/views.py
+++ music/views.py
@@ -5,33 +5,45 @@ import random
 
 
 def random_deck(request):
-    deck = random.choice()
-    return render(request, 'music/index.html', {'decks': deck})
+    decks = Deck.objects.all()
+    deck = random.choice(decks)
+    songs = Song.objects.all()
+    song = random.choice(songs)
+    return render(request, 'music/index.html', {'deck': deck, 'song': song})
+
 
 def random_song(request):
-    song = random.choice()
-    return render(request, 'music/index.html', {'songs': song})
+    songs = Song.objects.all()
+    song = random.choice(songs)
+    return render(request, 'music/index.html', {'song': song})
 
+
+# THIS IS WHERE THE ERROR IS SHOWING AN ISSUE
 def deck_list(request):
     decks = Deck.objects.all()
-    return render(request 'music/index.html', {'decks': deck})
+    return render(request, 'music/index.html', {'decks': decks})
+
 
 def artist_list(request):
-    artists= Artist.objects.all()
+    artists = Artist.objects.all()
     return render(request, 'music/artist_list.html', {'artists': artists})
+
 
 def album_list(request):
     albums = Album.objects.all()
     return render(request, 'music/album_list.html', {'albums': albums})
 
+
 def album_detail(request, pk):
     album = get_object_or_404(Album, pk=pk)
     return render(request, 'music/album_detail.html', {'album': album})
+
 
 def delete_album(request, pk):
     album = get_object_or_404(Album, pk=pk)
     album.delete()
     return redirect('album_list')
+
 
 def edit_album(request, pk):
     album = get_object_or_404(Album, pk=pk)
@@ -42,6 +54,7 @@ def edit_album(request, pk):
         form.save()
         return redirect('album-detail', pk=pk)
     return render(request, 'music/edit_album.html',  {'form': form})
+
 
 def create_album(request):
     if request.method == 'GET':
